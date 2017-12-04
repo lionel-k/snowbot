f03f40a112661d2812a3103ea3e16a25c6404aec
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-04 17:20:24 +0100

    created the model Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204161427) do
+ActiveRecord::Schema.define(version: 20171204161650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,23 @@ ActiveRecord::Schema.define(version: 20171204161427) do
     t.integer "snow_depth_high"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "guests_number"
+    t.string "checkin"
+    t.string "checkout"
+    t.string "start_city"
+    t.string "drivy_data"
+    t.string "homeaway_data"
+    t.bigint "domain_id"
+    t.integer "total_price"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["domain_id"], name: "index_orders_on_domain_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +60,6 @@ ActiveRecord::Schema.define(version: 20171204161427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "domains"
+  add_foreign_key "orders", "users"
 end
