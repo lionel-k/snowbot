902c408eac416c47983e7610d96ee123c53714dc
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-04 17:50:54 +0100

    migration to add column location in domains

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204161650) do
+ActiveRecord::Schema.define(version: 20171204164936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171204161650) do
     t.integer "snow_depth_high"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   create_table "orders", force: :cascade do |t|
