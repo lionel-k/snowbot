ff877fe8eea1090ec486e4b1510e44b16fe67fee
anthony hamou <anthonyhamou@yahoo.fr>
2017-12-05 18:31:22 +0100

    search-cars function

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'json'
+
+
 class OffersController < ApplicationController
   def index
     # 1. Search for cheapest cars on Drivy
@@ -5,13 +9,10 @@ class OffersController < ApplicationController
     # 3. Find the best domains from the input of the user
     # 4. For each of the best domains, find the cheapest flat
     # 5. From the cars and the flats, create a package which a list of 3 offers
+    # search_cars(params[:la_query_1])
+    @result = search_cars("Paris", "yes","12+Rue+du+Bac+Paris+France", "08%3A00","20%3A00", "2017-12-20","2017-12-22", 2000, "FR", "family")
   end
 
-  def search_cars
-    # return the cheapest cars found on Drivy
-    # input: location of the user, checkin at 8:00, checkout 20:00, distance: 2000kms, family car, instant booking
-    # output: 3 instances of object Car
-  end
 
   def update_weather_conditions
     # https://www.skiinfo.fr/france/bulletin-neige.html
@@ -31,5 +32,14 @@ class OffersController < ApplicationController
 
   def build_package
     # With a given set of cars and flats, return an array of [car, flat]
+  end
+
+  private
+  def search_cars(city_start, instant_booking, location, checkin, checkout, date_start, date_end, distance_max, country, car_type)
+    # return the cheapest cars found on Drivy
+    # input: location of the user, checkin at 8:00, checkout 20:00, distance: 2000kms, family car, instant booking
+    # output: 3 instances of object Car
+    url_drivy = "https://www.drivy.com/search.json?instant_bookable=#{instant_booking}&address_source=google&start_time=#{checkin}&country_scope=#{country}&start_date=#{date_start}&latitude=&car_types%5B%5D=#{car_type}&longitude=&end_time=#{checkout}&end_date=#{date_end}&address=#{location}&distance=#{distance_max}&city_display_name=#{city_start}"
+    @cars =JSON.parse(open(url_drivy).read)
   end
 end
