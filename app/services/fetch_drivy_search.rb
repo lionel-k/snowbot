8fca299e8cbf3b7a4af384f5f3390ac204ae466d
anthony hamou <anthonyhamou@yahoo.fr>
2017-12-06 18:32:10 +0100

    change FetchDrivySearch method

--- app/services/fetch_drivy_search.rb
+++ app/services/fetch_drivy_search.rb
@@ -2,59 +2,50 @@ require 'json'
 require 'open-uri'
 
 class FetchDrivySearch
+  def initialize(attributes = {})
+    @attributes = attributes
+  end
 
-  def initialize( attributes = {
-  checkin: checkin,
-  checkout: checkout,
-  location: location,
-  address: address
-  })
-
+  def call
     base_url_search = 'https://www.drivy.com/search.json?'
 
-    query_params = {
+    query_params_1 = {
       address_source: 'google',
-      start_time: '08%3A00',
+      start_time: '08:00',
       country_scope: 'FR',
-      start_date: attributes[:checkin],
-      latitude: attributes[:address].latitude,
-      car_types: 'family',
-      ## il manque %5B%5D
-      longitude: attributes[:address].longitude,
-      end_time: '20%3A00',
-      end_date: attributes[:checkout],
-      address: attributes[:address],
-      distance: '4000',
-      city_display_name: attributes[:location]
+      start_date: @attributes[:checkin]
+      #latitude: @attributes[:address].latitude
+    }
+
+    query_params_2 = {
+      #longitude: @attributes[:address].longitude,
+      end_time: '20:00',
+      end_date: @attributes[:checkout],
+      address: @attributes[:address],
+      distance: '2000'
     }
 
     url_drivy = base_url_search + 'instant_bookable=yes'
-
-    query_params.each do |k, v|
-      url_drivy += '&' + k.to_s + '=' + v
+    query_params_1.each do |k, v|
+      url_drivy += '&' + k.to_s + '=' + v.to_s
+    end
+    url_drivy += '&' + 'car_types[]=family'
+    query_params_2.each do |k, v|
+      url_drivy += '&' + k.to_s + '=' + v.to_s
     end
 
     listings_serialized = open(url_drivy).read
     listings = JSON.parse(listings_serialized)
     available_cars = listings['cars']
+    three_lowest_cars = available_cars.sort_by {|k| k['humanPrice'] }.first(3)
 
-    best_rated_available_cars = []
-    available_cars.each do |car|
-      if car['humanPrice'].to_f < 200
-        best_rated_available_cars << car
-      end
+    three_lowest_cars.each do |car|
+      Car.new(
+        id_drivy: car['id'],
+        price: car['humanPrice'],
+        title: car['carTitle'],
+        photo: car['carThumbUrl']
+      )
     end
-
-    car = best_rated_available_cars[0]
-
-    returned_car = Car.new(
-      id_drivy: car['id'],
-      price: car['humanPrice'],
-      title: car['carTitle'],
-      photo: car['carThumbUrl']
-    )
-
-    returned_car
-
   end
 end
