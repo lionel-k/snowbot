e3d1f250c81fc59cc8151522443ce01310ba70c3
anthony hamou <anthonyhamou@yahoo.fr>
2017-12-08 12:57:25 +0100

    autocomplete added

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'sidekiq-failures', '~> 1.0'
 gem 'money-rails'
 gem 'stripe'
 
+gem 'webpacker'
+
 
 group :development, :test do
   gem 'pry-byebug'
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,12 @@
+import "bootstrap";
+// import "../plugins/flatpickr";
+// import '../components/select2';
+// import '../components/guest_form_input';
+// import '../components/totalPrice';
+// import '../components/tabs';
+// import "dashboard";
+
+// import { loadDynamicBannerText } from '../components/banner';
+// loadDynamicBannerText();
+import { autocomplete } from '../components/autocomplete';
+autocomplete();
