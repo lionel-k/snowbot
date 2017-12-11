8763ef6eb3209f391bc940d3472a6f4909b72d84
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-12 00:31:24 +0100

    first version of the show of the offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,5 @@
 class OffersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:checkin].first.blank? || params[:checkout].first.blank? || params[:start_city].blank?
@@ -7,7 +7,7 @@ class OffersController < ApplicationController
     else
       @checkin = Date.parse(params[:checkin].first)
       @checkout = Date.parse(params[:checkout].first)
-      @diff_days = @checkout - @checkin
+      @diff_days = @checkout.mjd - @checkin.mjd
       @guests_number = params[:guests_number]
       offers_service = CreateOffersService.new(
         mountain_chain: params[:mountain_chain],
@@ -20,5 +20,14 @@ class OffersController < ApplicationController
       @offers = offers_service.call
       @order = Order.new
     end
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+    @order = Order.new
+    @checkin = Date.parse(@offer.user.query["checkin"])
+    @checkout = Date.parse(@offer.user.query["checkout"])
+    @guests_number = @offer.user.query["guests_number"]
+    @diff_days = @checkout.mjd - @checkin.mjd
   end
 end
