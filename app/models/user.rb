3e9589c135223f55fc39652d15be00741bd53466
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-12-12 16:45:38 +0100

    removed the possibility to recover a password - not useful

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, #:registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+  # :recoverable,
+  :rememberable, :trackable, :validatable
 
   devise :omniauthable, omniauth_providers: [:facebook]
 
